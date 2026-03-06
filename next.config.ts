19b2c836dfc8473fa2be2a302ec7a5ddf1cd8cf3
Dilip Kumawat <dilipkumawat785188@gmail.com>
2026-03-06 16:00:51 +0530

    Configure for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/PORTFOLIO" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/PORTFOLIO/" : "",
 };
 
 export default nextConfig;
